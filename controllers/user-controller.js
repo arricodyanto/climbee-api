cd7375f785c168170d09d89382aa2b07a6dec51c
arricodyanto <arricohandyanto@gmail.com>
2021-12-02 03:55:16 +0700

    conn

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,37 +13,36 @@ const profile = async(req, res, next) => {
 }
 
 const list = async(req, res, next) => {
-    const users = await db.User.findAll()
-    res.json({
-        success: true,
-        message: "Success Retrieve All User Data",
-        data: users,
-    })
-}
-
-// // const update = async(req, res, next) => {
-// //     try {
-// //         const user = req.body
-// //         const salt = bcrypt.genSaltSync(saltRounds)
-// //         const hash = bcrypt.hashSync(user.password, salt)
-// //             // const checkPassword = bcrypt.compareSync(users.password, hash)
-// //         user.password = hash
-// //         const insertData = await db.User.create(user)
-// //         insertData.password = undefined
-// //         res.status(201).json({
-// //             // message: "Welcome to Register ! (controller)"
-// //             success: true,
-// //             message: "Success Registering a New User !",
-// //             data: insertData
-// //         })
-// //     } catch (error) {
-// //         if (error.message === 'Validation error') {
-// //             next(ApiError.badRequest("Email or Username has been already used!"))
-// //         } else {
-// //             next(error)
-// //         }
-// //     }
-// // }
+        const users = await db.User.findAll()
+        res.json({
+            success: true,
+            message: "Success Retrieve All User Data",
+            data: users,
+        })
+    }
+    // // const update = async(req, res, next) => {
+    // //     try {
+    // //         const user = req.body
+    // //         const salt = bcrypt.genSaltSync(saltRounds)
+    // //         const hash = bcrypt.hashSync(user.password, salt)
+    // //             // const checkPassword = bcrypt.compareSync(users.password, hash)
+    // //         user.password = hash
+    // //         const insertData = await db.User.create(user)
+    // //         insertData.password = undefined
+    // //         res.status(201).json({
+    // //             // message: "Welcome to Register ! (controller)"
+    // //             success: true,
+    // //             message: "Success Registering a New User !",
+    // //             data: insertData
+    // //         })
+    // //     } catch (error) {
+    // //         if (error.message === 'Validation error') {
+    // //             next(ApiError.badRequest("Email or Username has been already used!"))
+    // //         } else {
+    // //             next(error)
+    // //         }
+    // //     }
+    // // }
 
 // const update = async(req, res, next) => {
 //     const { id } = req.user
